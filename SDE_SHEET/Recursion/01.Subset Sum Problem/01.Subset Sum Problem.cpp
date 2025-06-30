794581513041c566878b905768c2e0253cd0ea44
SatwikDubey <dubeysatwik0@gmail.com>
2025-06-30 22:39:16 +0530

    Subset

---
+++ SDE_SHEET/Recursion/01.Subset Sum Problem/01.Subset Sum Problem.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool check(int i,int sum,vector <int> &arr)
+    {
+        if (sum==0) return true;
+        if (sum <0 || i==arr.size()) return false;
+        if (check(i+1,sum-arr[i],arr)) return true;
+        if (check(i+1,sum,arr)) return true;
+        return false;
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        return check (0,sum,arr);
+    }
+};
