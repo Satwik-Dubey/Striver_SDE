4f213f575a192e091f0b8c75a20fa510de17f868
SatwikDubey <dubeysatwik0@gmail.com>
2025-08-13 00:18:56 +0530

    Create Kth smallest element in array.cpp

---
+++ SDE_SHEET/06.Heaps/01.Kth Smallest/Kth smallest element in array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int>pq;
+        for(int i=0;i<arr.size();i++){
+            pq.push(arr[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
