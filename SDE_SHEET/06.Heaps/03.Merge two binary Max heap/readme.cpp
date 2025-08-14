ba3eb08d04d861bc240fc87616599434715a55a3
SatwikDubey <dubeysatwik0@gmail.com>
2025-08-14 15:31:16 +0530

    Create readme.cpp

---
+++ SDE_SHEET/06.Heaps/03.Merge two binary Max heap/readme.cpp
@@ -0,0 +1,9 @@
+# Complexity
+
+Combining vectors: O(n + m)
+
+Heapifying with make_heap: O(n + m)
+
+Total Time Complexity: O(n + m)
+
+Space Complexity: O(1) extra space (apart from output vector)
